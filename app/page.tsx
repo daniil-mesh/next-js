0c8c0a79e563d5eb01be62658c516347d3b4dfeb
Daniil Meshcheriakov <sacretsid@gmail.com>
2024-05-03 21:30:50 +0400

    Prepare to run

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
+  const a = 4;
+  console.log(a);
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -15,7 +17,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
